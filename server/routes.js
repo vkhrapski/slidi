acfd82d5ab37eb4a8706b2d6daa3f4ee9f341d2c
Vladislav Khrapski <vkhrapski@gmail.com>
2015-07-30 19:51:30 +0300

    add presentation route

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/presentations', require('./api/presentation'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
